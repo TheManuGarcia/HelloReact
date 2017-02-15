51eb4b30bd25d2a41495f56cc58f8dcfee1a699b
ManuGarcia <ReservoirDog@ManuGarcias-MacBook-Pro-2.local>
2017-02-14 23:40:49 -0600

    adding first react component

--- public/app.jsx
+++ public/app.jsx
@@ -1,4 +1,21 @@
+//Common naming convention for react components (First letter is in Caps then camel case)
+var Greeter  = React.createClass({
+   render: function(){
+       return(
+           <div>
+               <h1>Hello React!</h1>
+               <p>This paragraph is from a component!</p>
+           </div>
+       );
+   }
+
+
+});
+
+//When you are returning JSX, you have to return only one root element, in this case <div>
+
 ReactDOM.render(
-    <h1>Hello React!</h1>,
+   //Passing our Greeter component using JSX
+    <Greeter/>,
     document.getElementById('app')
 );
