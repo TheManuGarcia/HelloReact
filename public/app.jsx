8f2ac263436bd7a67fb9533c6fb8b8542c34bf3c
ManuGarcia <ReservoirDog@ManuGarcias-MacBook-Pro-2.local>
2017-02-15 22:09:11 -0600

    added properties (props) and use getDefaultProps method

--- public/app.jsx
+++ public/app.jsx
@@ -1,10 +1,27 @@
 //Common naming convention for react components (First letter is in Caps then camel case)
 var Greeter  = React.createClass({
+
+    //getDefaultProps is a method of React that returns an object of properties
+
+    getDefaultProps: function(){
+
+        return {
+
+            name: 'React',
+            message: 'This is a message from the component'
+      }
+    },
    render: function(){
+
+       //Access our prop to display || this.props.object stores all of our props.
+       var name = this.props.name;
+       var message = this.props.message;
+
+
        return(
            <div>
-               <h1>Hello React!</h1>
-               <p>This paragraph is from a component!</p>
+               <h1>Hello {name}!</h1>
+               <p>{message}!</p>
            </div>
        );
    }
@@ -12,10 +29,14 @@ var Greeter  = React.createClass({
 
 });
 
+
 //When you are returning JSX, you have to return only one root element, in this case <div>
 
+var firstName = 'Manu';
+var message = 'Winter is coming';
+
 ReactDOM.render(
-   //Passing our Greeter component using JSX
-    <Greeter/>,
+   //Passing our Greeter component using JSX. || Passing in a property (prop) into our Greeter call.
+    <Greeter name={firstName} message={message}/>,
     document.getElementById('app')
 );
