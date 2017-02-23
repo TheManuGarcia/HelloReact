b292ff3e891d1aeaf9b7ff32d7cfd925dafca192
TheManuGarcia <jmanuel.gtez@gmail.com>
2017-02-23 15:35:55 -0600

    added GreeterMessage independent component

--- public/app.jsx
+++ public/app.jsx
@@ -3,20 +3,7 @@ var ReactDOM = require('react-dom');
 
 //Creating Form and Message component
 
-// PRESENTATIONAL COMPONENT
-var GreeterMessage = React.createClass({
-   render: function () {
-       var name = this.props.name;
-       var message = this.props.message;
-       return (
-         <div>
-             <h1>Hello {name} !</h1>
-             <p>{message}</p>
-         </div>
-       );
 
-   }
-});
 // PRESENTATIONAL COMPONENT
 
 var GreeterForm = React.createClass({
